831542e1242b4c3ce1568f8eb60e35674f60630e
jycurien <curien.jeanyves@gmail.com>
2023-03-26 14:57:23 +0200

    sending notification email

--- src/Service/ArticleCreator.php
+++ src/Service/ArticleCreator.php
@@ -5,13 +5,22 @@ namespace App\Service;
 use App\DTO\ArticleDto;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ArticleCreator
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly MailerInterface $mailer,
+    )
     {
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     */
     public function create(ArticleDto $articleDto): Article
     {
         $article = new Article();
@@ -22,6 +31,15 @@ class ArticleCreator
 
         $this->entityManager->persist($article);
         $this->entityManager->flush();
+
+        $email = (new Email())
+            ->from('my_blog@example.com')
+            ->to('admin@example.com')
+            ->subject('New article created')
+            ->text('A new article has been created: ' . $article->getTitle())
+        ;
+
+        $this->mailer->send($email);
 
         return $article;
     }
